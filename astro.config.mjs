89e047551728ad9a3e8ae72ed4ef4e162f73d2da
Ozy Wu Li <ozy.wu.li@gmail.com>
2023-12-03 16:50:45 -0800

    add new workflow file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://ozywuli.github.io',
+  base: '/ab_hype_doc',
+});
